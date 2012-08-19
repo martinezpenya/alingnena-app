eadb248f76df2e9d490879290fdfc8e8892a2e85
martinezpenya <david@martinezpenya.es>
2012-08-19 19:14:19 +0200

    Initial commit, empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlingnenaApp::Application.config.secret_token = '21f396a3ea6d2ea61f55b92dbd2f006cbbae877148f4754c84c74f1cf72e4e5c21bcebe4e2bef9fa324417cf736e2e76a62937ee5feb68d2d9817ac898091849'
